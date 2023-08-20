b9fd5a10141ee9463d4f394733e6fd0268ab2a15
shanu singh <krksingh.99@gmail.com>
2023-08-20 15:27:02 +0530

    its a basic form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, {useState} from 'react'
+const App=()=>{
+  const form={
+    Name:"",
+    Email:"",
+    Subject:"",
+    Message:"",
+  }
+  const [details,setDetails]=useState(form);
+  const updateForm=(e,type)=>{
+    setDetails({...details,[type]:e.target.value})
+  }
+  console.log(details)
+  return(
+    <div className='app-container'>
+      <div className="header">
+         Feedback Form
+       
+      </div>
+      <div className="middle">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="form">
+        <label>Name:</label>
+        <input type="text" placeholder="Enter Name" onChange={(e)=>updateForm(e,'Name')}/>
+        <label>Email:</label>
+        <input type="text" placeholder="Enter email"  onChange={(e)=>updateForm(e,'Email')}/>
+        <label>Subject:</label>
+        <input type="text" placeholder="Enter subject"  onChange={(e)=>updateForm(e,'Subject')}/>
+        <label>Message:</label>
+        <input type="text" placeholder="Enter message"  onChange={(e)=>updateForm(e,'Message')}/>
+        </div>
+        <button>Submit</button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
